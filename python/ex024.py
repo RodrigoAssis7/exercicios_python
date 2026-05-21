347b170962e09d1cf6862a8fd96c106a8e6fa4d1
Rodrigo Assis <assisrodrigo7741@gmail.com>
2026-05-21 15:17:43 -0300

    alteração 21/06/2024 17:30:00

--- python/ex024.py
+++ python/ex024.py
@@ -1,1 +1,4 @@
-print("hello world")
+cidade = input("Digite o nome da cidade:")
+if cidade == "Santo Antônio":
+    print("A cidade começa com Santo")
+else:    print("A cidade não começa com Santo")  
